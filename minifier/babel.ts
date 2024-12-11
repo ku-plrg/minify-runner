795d3d68aa4eb4fc211211c12877e16db50842a9
Kanguk Lee <p51lee@kaist.ac.kr>
2024-12-11 22:31:51 +0900

    Support babel

--- minifier/babel.ts
+++ minifier/babel.ts
@@ -1,7 +1,12 @@
+import * as path from "https://deno.land/std@0.220.1/path/mod.ts";
+import { ensureDir } from "https://deno.land/std@0.220.1/fs/ensure_dir.ts";
+import { exists } from "https://deno.land/std@0.220.1/fs/exists.ts";
+import { getCacheDir } from "~/misc/cache.ts";
+
 interface BabelModule {
-  transpile(
+  transform(
     code: string,
-    config: Record<string, any>,
+    options?: Object,
   ): Promise<{ code: string }>;
 }
 
@@ -9,20 +14,40 @@ export async function load(version: string) {
   return getCachedModule(version);
 }
 
+async function fetchAndCache(url: string, cachePath: string) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+  }
+  const content = await response.text();
+  await Deno.writeTextFile(cachePath, content);
+  return content;
+}
+
 export async function transform({ code, config, babel }: {
   code: string;
   config: Record<string, any>;
   babel: BabelModule;
 }) {
-  const transpiledCode = await babel.transpile(code, config);
+  const transpiledCode = await babel.transform(code, config);
   return transpiledCode.code.trim();
 }
 
 // TODO: Implement cache
 async function getCachedModule(version: string): Promise<BabelModule> {
   // for version >= 7.14.0: https://cdn.jsdelivr.net/npm/@babel/standalone@7.14.0/babel.min.js
-  const loaderPath =
-    `https://cdn.jsdelivr.net/npm/@babel/standalone@${version}/babel.min.js`;
-  const { transpile } = await import(loaderPath);
-  return { transpile };
+  const cacheDir = `${getCacheDir()}/babel/${version}`;
+  const loaderCachePath = path.join(cacheDir, "index.js");
+
+  const loaderUrl =
+    `https://cdn.jsdelivr.net/npm/@babel/standalone@${version}/+esm`;
+
+  await ensureDir(cacheDir);
+
+  if (!(await exists(loaderCachePath))) {
+    await fetchAndCache(loaderUrl, loaderCachePath);
+  }
+
+  const { transform } = await import(loaderCachePath);
+  return { transform };
 }
